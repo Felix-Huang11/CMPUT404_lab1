d2f9b925d4dffbb21136864777a363f1316a6599
Z Huang <zhuang10@ualberta.ca>
2021-01-12 10:27:15 +0800

    modify

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ import requests
 print(requests.__version__)
 r = requests.get(http://www.google.com)
 print(r.text)
+
+raw = requests.get("https://raw.githubusercontent.com/Felix-Huang11/CMPUT404_lab1/main/script.py")
+print(raw.text)
